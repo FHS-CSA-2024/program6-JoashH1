8b7cf6d38095f2863ee16aa8b57e060edf5a89fe
JoashH1 <hernandezjoash6@gmail.com>
2024-08-24 15:52:53 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,9 +1,18 @@
 //import stuff here!
 
 //Your code here
-
-//Paste console output below:
-/*
-
-
+class Program6{
+    public static void main(String[] args) {
+     double circumference = 22.272;
+     double radius = 3.712;
+     double diameter = 7.424;
+     double area = 41.337;
+    
+//Output values
+     System.out.println( "Mushroom Cup Prix Racer Average Miles/Per Gallon");
+     System.out.println( "Royale averaged " + royaleMPG  +" m/g");
+     System.out.println( "Koopa King averaged " + koopaMPG +" m/g");
+     System.out.println( "Pipe Frame averaged " + frameMPG +" m/g");
+     System.out.println( "BadWagon averaged " + BadwagonMPG +" m/g");
+    }
 */
